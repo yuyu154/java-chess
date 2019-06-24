b92a46d997fef06d8cb1ef7a043523668afca017
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-06-24 23:53:09 +0900

    hotfix. 컴파일 오류 수정 및 PositionConverter 매직넘버 -> 상수

--- src/main/java/chess/utils/PositionConverter.java
+++ src/main/java/chess/utils/PositionConverter.java
@@ -1,18 +1,20 @@
 package chess.utils;
 
 import chess.domain.Position;
-import chess.exception.PositionConvertException;
 
 public class PositionConverter {
 	private static final String DELIMITER = "";
+	private static final int POSITION_LENGTH = 2;
+	private static final int COLUMN_INDEX = 0;
+	private static final int ROW_INDEX = 1;
 
 	public static Position convert(final String input) {
 		String[] position = input.split(DELIMITER);
-		if (position.length != 2) {
+		if (position.length != POSITION_LENGTH) {
 			throw new IllegalArgumentException();
 		}
-		String col = position[0];
-		String row = position[1];
+		String col = position[COLUMN_INDEX];
+		String row = position[ROW_INDEX];
 		return Position.of(row, col);
 	}
 }
