7e950ad24d9c5c522d87573bd1582fcd010e51f2
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-11-13 23:31:29 +0900

    test: RoomRepository Test 개선

--- src/test/java/chess/dao/RoomRepositoryTest.java
+++ src/test/java/chess/dao/RoomRepositoryTest.java
@@ -1,43 +1,23 @@
 package chess.dao;
 
 import chess.domain.entity.Room;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceContext;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RoomRepositoryTest {
-
-    @PersistenceContext
-    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpachess");
-
     private RoomRepository roomRepository;
 
     @BeforeEach
     void setUp() {
         roomRepository = new RoomRepository();
-        roomRepository.deleteAll();
-    }
-
-    @Test
-    void add() {
-        roomRepository.add();
-        roomRepository.add();
-        assertThat(roomRepository.getLatestId().get()).isEqualTo(2L);
     }
 
     @Test
     void findById() {
-        roomRepository.add();
         Room room = roomRepository.add();
         Room actual = roomRepository.findById(room.getId())
                 .get();
@@ -54,21 +34,7 @@ class RoomRepositoryTest {
 
     @Test
     void getLatestId() {
-        roomRepository.add();
-        roomRepository.add();
-        assertThat(roomRepository.getLatestId().get()).isEqualTo(2L);
-    }
-
-    @Test
-    void deleteAll() {
-        roomRepository.add();
-        roomRepository.add();
-        roomRepository.deleteAll();
-        assertThat(roomRepository.findAllByStatus(0)).isEqualTo(0);
-    }
-
-    @AfterEach
-    void tearDown() {
-        roomRepository.deleteAll();
+        Room room = roomRepository.add();
+        assertThat(roomRepository.getLatestId().get()).isEqualTo(room.getId());
     }
 }
